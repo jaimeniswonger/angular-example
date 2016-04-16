2f6139da5021c96e536de7c963b81ac27225ac0a
Braden Niswonger <bradenniswonger@gmail.com>
2016-04-15 19:41:36 -0500

    cleaned up

--- app/app.js
+++ app/app.js
@@ -5,6 +5,8 @@ angular.module('currencyCounter', [])
     .controller('CurrencyCounterCtrl', ['$scope',
         function ($scope) {
 
+            $scope.quarters = $scope.dimes = $scope.nickels = $scope.pennies = 0;
+
             $scope.totalIt = function () {
                 $scope.total = $scope.quarters * .25 + $scope.dimes * .10 + $scope.nickels * .05 + $scope.pennies * .01;
             }
